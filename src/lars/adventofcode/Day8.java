92155b1373c93ca42ab16cbd1f9c291e4cdbb516
Lars Jølsum <github@jolsum.net>
2015-12-11 23:46:40 +0100

    Day 8 part two

--- src/lars/adventofcode/Day8.java
+++ src/lars/adventofcode/Day8.java
@@ -9,14 +9,20 @@ public class Day8 {
 
 	private static int totChars = 0;
 	private static int totMem = 0;
+	private static int totEscaped = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		try (Stream<String> lines = Files.lines(Paths.get("input", "8.txt"))) {
-			lines.forEach(l -> handleLine(l));
+			lines.forEach(l -> {
+				handleLine(l);
+				handleLine2(l);
+			});
+
 		}
 
 		System.out.println(totChars + " - " + totMem + " = " + (totChars - totMem));
+		System.out.println(totEscaped + " - " + totChars + " = " + (totEscaped - totChars));
 
 	}
 
@@ -28,6 +34,15 @@ public class Day8 {
 
 		totChars += line.length();
 		totMem += newLine.length();
+	}
+
+	private static void handleLine2(final String line) {
+		String newLine = line;
+		newLine = newLine.replace("\\", "\\\\"); // \ -> \\
+		newLine = newLine.replace("\"", "\\\""); // " -> \"
+		newLine = "\"" + newLine + "\"";
+
+		totEscaped += newLine.length();
 	}
 
 }
