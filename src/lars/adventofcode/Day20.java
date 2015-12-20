e4440317c15daaf4b0b371778b85201dd94bfc47
Lars Jølsum <github@jolsum.net>
2015-12-20 20:31:11 +0100

    Day 20

---
+++ src/lars/adventofcode/Day20.java
@@ -0,0 +1,27 @@
+package lars.adventofcode;
+
+public class Day20 {
+
+	public static void main(String[] args) {
+
+		for (int house = 1;; house++) {
+			int presents = 0;
+			for (int elf = 1; elf <= house; elf++) {
+				if (house % elf == 0) {
+					presents += elf * 10;
+				}
+			}
+
+			if (house % 10000 == 0) {
+				System.out.println("House " + house + " got " + presents + " presents");
+			}
+
+			if (presents == 29000000) {
+				System.out.println("House " + house + " got " + presents + " presents");
+				break;
+			}
+		}
+
+	}
+
+}
